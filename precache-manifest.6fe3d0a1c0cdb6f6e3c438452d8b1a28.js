a4dca461ab66c9d554c2fff952559eae888f01df
Soumen Dey <soumen1999souraj@gmail.com>
2020-08-10 07:41:41 +0530

    Updates

---
+++ precache-manifest.6fe3d0a1c0cdb6f6e3c438452d8b1a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb47c5bdc6509eafdf84321162b83d1",
+    "url": "/Devstagram/index.html"
+  },
+  {
+    "revision": "646cd462a748b5e9bd36",
+    "url": "/Devstagram/static/css/main.3bc4b44c.chunk.css"
+  },
+  {
+    "revision": "ee80e99308561ac2ad74",
+    "url": "/Devstagram/static/js/2.6bec2f5b.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/Devstagram/static/js/2.6bec2f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646cd462a748b5e9bd36",
+    "url": "/Devstagram/static/js/main.5a2d0efa.chunk.js"
+  },
+  {
+    "revision": "5d4ec8309ae54a3d4a44",
+    "url": "/Devstagram/static/js/runtime-main.5fc3682a.js"
+  }
+]);
